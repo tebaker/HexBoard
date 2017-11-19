35820be5eed7104580e9bb7289f8aa1bf362fc46
Talon Baker <taalonn@gmail.com>
2017-11-18 23:36:42 -0800

    Added drop shadows to board added pieces to board added drop shadow to pieces

---
+++ Hello/DrawingTools.h
@@ -0,0 +1,398 @@
+/*
+ * DrawingTools.cpp
+ *
+ *  Created on: Nov 17, 2017
+ *      Author: Talon
+ */
+
+#ifndef DRAWINGTOOLS_H_
+#define DRAWINGTOOLS_H_
+
+#include "HexBoard.h"
+#include <windows.h>  // For MS Windows
+#include <GL/glut.h>  // GLUT, includes glu.h and gl.h
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+const float R          = 0.065;
+const float PI         = 3.141592653589;
+const float X_OFFSET   = R * cos(PI / 6);
+const float Y_OFFSET   = R + R / 2;
+const int   BOARD_SIZE = 11;
+const float MASTER_X   = -0.85;
+const float MASTER_Y   =  0.85;
+
+void drawPiece(float x, float y, tileStatus color) {
+	//resetting radius so the piece will be smaller than the hexagon
+	float cirR = R - 0.025;
+
+	//off setting x, y so shadow will be at bottom left
+	x -= 0.005;
+	y -= 0.005;
+
+	//drawing a CIRCLE to the screen
+	glBegin(GL_POLYGON);					//starting drawing boarder
+	//piece shadow same gray as the board shadow
+	glColor3f(0.794f, 0.766f, 0.790f);
+		//N
+		glVertex2f(x + cirR * 0.0,           y + cirR * 1.0);
+		//Quadrant II
+		glVertex2f(x - cirR * 0.5,           y + cirR * (sqrt(3) / 2));
+		glVertex2f(x - cirR * (sqrt(2) / 2), y + cirR * (sqrt(2) / 2));
+		glVertex2f(x - cirR * (sqrt(3) / 2), y + cirR * 0.5);
+		//W
+		glVertex2f(x - cirR * 1.0,           y + cirR * 0.0);
+		//Quadrant III
+		glVertex2f(x - cirR * (sqrt(3) / 2), y - cirR * 0.5);
+		glVertex2f(x - cirR * (sqrt(2) / 2), y - cirR * (sqrt(2) / 2));
+		glVertex2f(x - cirR * 0.5,           y - cirR * (sqrt(3) / 2));
+		//S
+		glVertex2f(x + cirR * 0.0,           y - cirR * 1.0);
+		//Quadrant IV
+		glVertex2f(x + cirR * 0.5,           y - cirR * (sqrt(3) / 2));
+		glVertex2f(x + cirR * (sqrt(2) / 2), y - cirR * (sqrt(2) / 2));
+		glVertex2f(x + cirR * (sqrt(3) / 2), y - cirR * 0.5);
+		//E
+		glVertex2f(x + cirR * 1.0,           y + cirR * 0.0);
+		//Quadrant I
+		glVertex2f(x + cirR * (sqrt(3) / 2), y + cirR * 0.5);
+		glVertex2f(x + cirR * (sqrt(2) / 2), y + cirR * (sqrt(2) / 2));
+		glVertex2f(x + cirR * 0.5,           y + cirR * (sqrt(3) / 2));
+
+	glEnd();							//ending drawing boarder
+
+	//resetting radius so the piece will be smaller than the shadow
+	cirR = R - 0.03;
+
+	//resetting x, y so piece will be at center of hexagon
+	x += 0.005;
+	y += 0.005;
+
+	//drawing a CIRCLE to the screen
+	glBegin(GL_POLYGON);					//starting drawing boarder
+
+		if(color == RED) {
+			//piece output RED
+			glColor3f(1.0f, 0.0, 0.0f);
+		}
+		else {
+			//otherwise BLUE
+			glColor3f(0.0f, 0.0, 1.0f);
+		}
+
+		//N
+		glVertex2f(x + cirR * 0.0,           y + cirR * 1.0);
+		//Quadrant II
+		glVertex2f(x - cirR * 0.5,           y + cirR * (sqrt(3) / 2));
+		glVertex2f(x - cirR * (sqrt(2) / 2), y + cirR * (sqrt(2) / 2));
+		glVertex2f(x - cirR * (sqrt(3) / 2), y + cirR * 0.5);
+		//W
+		glVertex2f(x - cirR * 1.0,           y + cirR * 0.0);
+		//Quadrant III
+		glVertex2f(x - cirR * (sqrt(3) / 2), y - cirR * 0.5);
+		glVertex2f(x - cirR * (sqrt(2) / 2), y - cirR * (sqrt(2) / 2));
+		glVertex2f(x - cirR * 0.5,           y - cirR * (sqrt(3) / 2));
+		//S
+		glVertex2f(x + cirR * 0.0,           y - cirR * 1.0);
+		//Quadrant IV
+		glVertex2f(x + cirR * 0.5,           y - cirR * (sqrt(3) / 2));
+		glVertex2f(x + cirR * (sqrt(2) / 2), y - cirR * (sqrt(2) / 2));
+		glVertex2f(x + cirR * (sqrt(3) / 2), y - cirR * 0.5);
+		//E
+		glVertex2f(x + cirR * 1.0,           y + cirR * 0.0);
+		//Quadrant I
+		glVertex2f(x + cirR * (sqrt(3) / 2), y + cirR * 0.5);
+		glVertex2f(x + cirR * (sqrt(2) / 2), y + cirR * (sqrt(2) / 2));
+		glVertex2f(x + cirR * 0.5,           y + cirR * (sqrt(3) / 2));
+
+	glEnd();							//ending drawing boarder
+}
+
+/* Handler for window-repaint event. Call back when the window first appears and
+   whenever the window needs to be re-painted. */
+void drawHexBoard(){
+	float currX = MASTER_X - 0.02;
+	float y     = MASTER_Y - 0.02;
+	float x     = currX;	//currX for line position when new line is started
+
+	//background color is opaque white
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	//setting initial line width
+	glLineWidth(1.0f);
+
+	//creating the drop shadow below board
+    for(int i = 0; i < BOARD_SIZE; i++) {
+		glBegin(GL_POLYGON);				//starting drawing polygon
+
+			//line RGB light purple-gray
+			glColor3f(0.794f, 0.766f, 0.790f);
+			//adding all points to hexagon starting with north-most point
+			glVertex2f(x + R * 0.0,           y + R * 1.0);
+			glVertex2f(x - R * (sqrt(3) / 2), y + R * 0.5);
+			glVertex2f(x - R * (sqrt(3) / 2), y - R * 0.5);
+			glVertex2f(x + R * 0.0,           y - R * 1.0);
+			glVertex2f(x + R * (sqrt(3) / 2), y - R * 0.5);
+			glVertex2f(x + R * (sqrt(3) / 2), y + R * 0.5);
+
+		glEnd();							//ending drawing polygon
+		//incrementing x by X_OFFSET
+		x += X_OFFSET;
+		y -= Y_OFFSET;
+	}//end - inner for
+
+	x = MASTER_X + 10  * X_OFFSET - 0.02;
+	y = MASTER_Y - 10 * Y_OFFSET - 0.02;
+
+	//creating the drop shadow below board
+    for(int i = 0; i < BOARD_SIZE; i++) {
+		glBegin(GL_POLYGON);				//starting drawing polygon
+
+			//line RGB light purple-gray
+			glColor3f(0.794f, 0.766f, 0.790f);
+			//adding all points to hexagon starting with north-most point
+			glVertex2f(x + R * 0.0,           y + R * 1.0);
+			glVertex2f(x - R * (sqrt(3) / 2), y + R * 0.5);
+			glVertex2f(x - R * (sqrt(3) / 2), y - R * 0.5);
+			glVertex2f(x + R * 0.0,           y - R * 1.0);
+			glVertex2f(x + R * (sqrt(3) / 2), y - R * 0.5);
+			glVertex2f(x + R * (sqrt(3) / 2), y + R * 0.5);
+
+		glEnd();							//ending drawing polygon
+		//incrementing x by X_OFFSET
+		x += 2 * X_OFFSET;
+	}//end - inner for
+
+    currX = MASTER_X;
+    	y     = MASTER_Y;
+    	x 	  = currX;		//currX for line position when new line is started
+
+    	//creating the background of the board
+        for(int i = 0; i < BOARD_SIZE; i++) {
+        	for(int j = 0; j < BOARD_SIZE; j++) {
+    			glBegin(GL_POLYGON);				//starting drawing polygon
+
+    				//line RGB light purple-gray
+    				glColor3f(0.937f, 0.921f, 0.937f);
+    				//adding all points to hexagon starting with north-most point
+    				glVertex2f(x + R * 0.0,           y + R * 1.0);
+    				glVertex2f(x - R * (sqrt(3) / 2), y + R * 0.5);
+    				glVertex2f(x - R * (sqrt(3) / 2), y - R * 0.5);
+    				glVertex2f(x + R * 0.0,           y - R * 1.0);
+    				glVertex2f(x + R * (sqrt(3) / 2), y - R * 0.5);
+    				glVertex2f(x + R * (sqrt(3) / 2), y + R * 0.5);
+
+        		glEnd();							//ending drawing polygon
+        		//incrementing x by X_OFFSET
+        		x += 2 * X_OFFSET;
+        	}//end - inner for
+        	//resetting x, y for new row
+        	currX += X_OFFSET;
+        	x = currX;
+        	y -= Y_OFFSET;
+        }//end - outer for
+
+	currX = MASTER_X;
+	y     = MASTER_Y;
+	x 	  = currX;		//currX for line position when new line is started
+
+	//creating the shadow for the pieces
+    for(int i = 0; i < BOARD_SIZE; i++) {
+    	for(int j = 0; j < BOARD_SIZE; j++) {
+			glBegin(GL_LINE_LOOP);				//starting drawing polygon
+
+				//slightly darker shadow for piece outline
+				glColor3f(0.894f, 0.866f, 0.890f);
+				//adding all points to hexagon starting with north-most point
+				glVertex2f(x + R * 0.0,           y + R * 1.0);	//N
+				glVertex2f(x - R * (sqrt(3) / 2), y + R * 0.5);	//NW
+				glVertex2f(x - R * (sqrt(3) / 2), y - R * 0.5);	//SW
+				glVertex2f(x + R * 0.0,           y - R * 1.0);	//S
+				glVertex2f(x + R * (sqrt(3) / 2), y - R * 0.5);	//SE
+				glVertex2f(x + R * (sqrt(3) / 2), y + R * 0.5); //NE
+
+    		glEnd();							//ending drawing polygon
+    		//incrementing x by X_OFFSET
+    		x += 2 * X_OFFSET;
+    	}//end - inner for
+    	//resetting x, y for new row
+    	currX += X_OFFSET;
+    	x = currX;
+    	y -= Y_OFFSET;
+    }//end - outer for
+
+	//resetting the current x, x, and y
+	x = MASTER_X;
+	y = MASTER_Y;
+
+	//setting thicker line width for red/blue output
+	glLineWidth(2.0f);
+
+	//creating the RED TOP boarder
+	for(int i = 0; i < BOARD_SIZE; i++) {
+		glBegin(GL_LINES);					//starting drawing boarder
+
+			//RED for TOP boarder
+			glColor3f(1.0f, 0.0f, 0.0f);
+			//adding all points top points to hexagon starting with northwest-most point
+			glVertex2f(x - R * (sqrt(3) / 2), y + R * 0.5);	//NW
+			glVertex2f(x + R * 0.0,           y + R * 1.0);	//N
+			glVertex2f(x + R * 0.0,           y + R * 1.0);	//N
+			glVertex2f(x + R * (sqrt(3) / 2), y + R * 0.5);	//NE
+
+		glEnd();							//ending drawing boarder
+		//incrementing x by X_OFFSET
+		x += 2 * X_OFFSET;
+	}//end - inner for
+
+	//resetting the current x, x, and y
+	x = MASTER_X;
+	y = MASTER_Y;
+
+	//manually setting x, y values: moving x five places to the right and y eleven places down
+	x += 10 * X_OFFSET;
+	y -= 10 * Y_OFFSET;
+
+	//creating the RED BOTTOM boarder
+	for(int i = 0; i < BOARD_SIZE; i++) {
+		glBegin(GL_LINES);					//starting drawing boarder
+
+			//RED for BOTTOM boarder
+			glColor3f(1.0f, 0.0f, 0.0f);
+			//adding all points top points to hexagon starting with northwest-most point
+			glVertex2f(x - R * (sqrt(3) / 2), y - R * 0.5);	//SW
+			glVertex2f(x + R * 0.0,           y - R * 1.0);	//S
+			glVertex2f(x + R * 0.0,           y - R * 1.0);	//S
+			glVertex2f(x + R * (sqrt(3) / 2), y - R * 0.5);	//SE
+
+		glEnd();							//ending drawing boarder
+		//incrementing x by X_OFFSET
+		x += 2 * X_OFFSET;
+	}//end - inner for
+
+	//resetting the current x, x, and y
+	x = MASTER_X;
+	y = MASTER_Y;
+
+	//creating the BLUE LEFT boarder
+	for(int i = 0; i < BOARD_SIZE; i++) {
+		glBegin(GL_LINES);					//starting drawing boarder
+
+			//BLUE for LEFT boarder color
+			glColor3f(0.0f, 0.0f, 1.0f);
+			//adding all points top points to hexagon starting with northwest-most point
+			glVertex2f(x - R * (sqrt(3) / 2), y + R * 0.5);	//NW
+			glVertex2f(x - R * (sqrt(3) / 2), y - R * 0.5);	//SW
+			glVertex2f(x - R * (sqrt(3) / 2), y - R * 0.5);	//SW
+			glVertex2f(x + R * 0.0,           y - R * 1.0);	//S
+
+		glEnd();							//ending drawing boarder
+		//incrementing x, y moving down on the left hand side
+		x += X_OFFSET;
+		y -= Y_OFFSET;
+	}//end - inner for
+
+	//resetting the current x, x, and y
+	x = MASTER_X;
+	y = MASTER_Y;
+
+	//manually setting x, y values: moving x 11 places to the right keeping y where it is
+	x += 20 * X_OFFSET;
+
+	//creating the BLUE RIGHT boarder
+	for(int i = 0; i < BOARD_SIZE; i++) {
+		glBegin(GL_LINES);					//starting drawing boarder
+
+			//BLUE for RIGHT boarder
+			glColor3f(0.0f, 0.0f, 1.0f);
+			//adding all points top points to hexagon starting with northwest-most point
+			glVertex2f(x + R * 0.0,           y + R * 1.0);	//N
+			glVertex2f(x + R * (sqrt(3) / 2), y + R * 0.5); //NE
+			glVertex2f(x + R * (sqrt(3) / 2), y + R * 0.5); //NE
+			glVertex2f(x + R * (sqrt(3) / 2), y - R * 0.5);	//SE
+
+		glEnd();							//ending drawing boarder
+		//incrementing x, y moving down on the left hand side
+		x += X_OFFSET;
+		y -= Y_OFFSET;
+	}//end - inner for
+
+
+	//resetting the current x, x, and y
+	x = MASTER_X;
+	y = MASTER_Y;
+
+	//manually setting x, y values: moving x five places to the right and y eleven places down
+	x += 10 * X_OFFSET;
+	y -= 10 * Y_OFFSET;
+
+	//accounting for the half-color (blue/red) at bottom left
+	glBegin(GL_LINES);					//starting drawing boarder
+		//PURPLE for SW and NE corners
+		glColor3f(0.4f, 0.1f, 0.7f);
+		//coloring the SW corner purple
+		glVertex2f(x - R * (sqrt(3) / 2), y - R * 0.5);	//SW
+		glVertex2f(x + R * 0.0,           y - R * 1.0);	//S
+	glEnd();							//ending drawing boarder
+
+	//resetting the current x, x, and y
+	x = MASTER_X;
+	y = MASTER_Y;
+
+	//manually setting x, y values: moving x five places to the right an0.396f, 0.070f, 0.709fd y eleven places down
+	x += 20 * X_OFFSET;
+
+	//accounting for the half-color (blue/red) at bottom left
+	glBegin(GL_LINES);					//starting drawing boarder
+		//PURPLE for SW and NE corners
+		glColor3f(0.4f, 0.1f, 0.7f);
+		//coloring the SW corner purple
+		glVertex2f(x + R * 0.0,           y + R * 1.0);	//N
+		glVertex2f(x + R * (sqrt(3) / 2), y + R * 0.5); //NE
+	glEnd();							//ending drawing boarder
+
+	//resetting the current x, x, and y
+	x = MASTER_X;
+	y = MASTER_Y;
+
+	float cirR = R - 0.03;
+
+	//drawing a CIRCLE to the screen
+	glBegin(GL_POLYGON);					//starting drawing boarder
+	//RED for fun
+	glColor3f(1.0f, 0.0f, 0.0f);
+		//N
+		glVertex2f(x + cirR * 0.0,           y + cirR * 1.0);
+		//Quadrant II
+		glVertex2f(x - cirR * 0.5,           y + cirR * (sqrt(3) / 2));
+		glVertex2f(x - cirR * (sqrt(2) / 2), y + cirR * (sqrt(2) / 2));
+		glVertex2f(x - cirR * (sqrt(3) / 2), y + cirR * 0.5);
+		//W
+		glVertex2f(x - cirR * 1.0,           y + cirR * 0.0);
+		//Quadrant III
+		glVertex2f(x - cirR * (sqrt(3) / 2), y - cirR * 0.5);
+		glVertex2f(x - cirR * (sqrt(2) / 2), y - cirR * (sqrt(2) / 2));
+		glVertex2f(x - cirR * 0.5,           y - cirR * (sqrt(3) / 2));
+		//S
+		glVertex2f(x + cirR * 0.0,           y - cirR * 1.0);
+		//Quadrant IV
+		glVertex2f(x + cirR * 0.5,           y - cirR * (sqrt(3) / 2));
+		glVertex2f(x + cirR * (sqrt(2) / 2), y - cirR * (sqrt(2) / 2));
+		glVertex2f(x + cirR * (sqrt(3) / 2), y - cirR * 0.5);
+		//E
+		glVertex2f(x + cirR * 1.0,           y + cirR * 0.0);
+		//Quadrant I
+		glVertex2f(x + cirR * (sqrt(3) / 2), y + cirR * 0.5);
+		glVertex2f(x + cirR * (sqrt(2) / 2), y + cirR * (sqrt(2) / 2));
+		glVertex2f(x + cirR * 0.5,           y + cirR * (sqrt(3) / 2));
+
+	glEnd();							//ending drawing boarder
+
+
+	//rendering all data to the screen
+	glFlush();
+}
+
+#endif /* DRAWINGTOOLS_H_ */
---
+++ Hello/main.cpp
@@ -0,0 +1,67 @@
+/* PROGRAMMER  : TALON BAKER
+ * CLASS       : CMPS 109
+ * HW_04       : HEX BOARD
+ * DUE DATE    : 11/10/17
+ */
+
+#include "HexBoard.h"
+#include "DrawingTools.h"
+#include <iostream>
+#include <vector>
+#include <random>
+#include <algorithm>	//needed for random_shuffle
+#include <chrono>
+#include <windows.h>    //for MS Windows
+#include <GL/glut.h>    //GLUT, includes glu.h and gl.h
+
+using namespace std;
+
+/* Main function: GLUT runs as a console application starting at main()  */
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);                 // Initialize GLUT
+	glutCreateWindow("Hex Board"); // Create a window with the given title
+	glutReshapeWindow(500, 500);
+
+	// obtain a time-based seed:
+	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+
+	HexBoard    board(BOARD_SIZE);
+	vector<int> randArray;
+
+	//populating an array with alternating REDs and BLUEs
+	for(int i = 0; i < BOARD_SIZE*BOARD_SIZE; i++) {
+		//using static_cast to safely convert from enum to int
+		if(i % 2 == 0) {
+			randArray.push_back(static_cast<int>(RED));
+		}
+		else {
+			randArray.push_back(static_cast<int>(BLUE));
+		}
+	}
+
+	//randomly shuffling array
+	shuffle(randArray.begin(), randArray.end(), std::default_random_engine(seed));
+
+	//adding pieces from randomly shuffled array to the board
+	for(int i = 0; i < BOARD_SIZE; i++) {
+		for(int j = 0; j < BOARD_SIZE; j++) {
+			board.addPiece(i, j, static_cast<tileStatus>(randArray.back()));
+			randArray.pop_back();
+		}
+	}
+
+	cout << board.printBoard();
+
+	cout << "Winner is: ";
+	if(board.calculateWinner() == BLUE) {
+		cout << "BLUE!"<< endl;
+	}
+	else {
+		cout << "RED!" << endl;
+	}
+
+	glutDisplayFunc(drawHexBoard); // Register display callback handler for window re-paint
+	glutMainLoop();           // Enter the infinitely event-processing loop
+
+	return 0;
+}//end - main
